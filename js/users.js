79112a2ff547c891a0822a711b2aa113b64212db
YuqiChou <zhouyuqi.master@gmail.com>
2017-01-15 21:40:27 +0800

    add test data

---
+++ js/users.js
@@ -0,0 +1,21 @@
+var members = [
+    {
+        "fileName": "1.jpg",
+        "memberName": "路人甲",
+        "fullName": "呵呵哒 路人甲"
+    },
+    {
+        "fileName": "2.jpg",
+        "memberName": "路人乙",
+        "fullName": "嘿嘿嘿 路人乙"
+    },
+    {
+        "fileName": "3.jpg",
+        "memberName": "路人丙",
+        "fullName": "哇咔咔 路人丙"
+    },
+    {
+        "fileName": "4.jpg",
+        "memberName": "路人丁",
+        "fullName": "哼哼哈呵 路人丁丁"
+    }];
